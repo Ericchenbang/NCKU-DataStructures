06291f96e1de48f069809c428a04ef4da64ac647
Ericchenbang <ericchenbsng@gmail.com>
2024-11-21 10:23:38 +0800

    Inorder to postorder

--- traversal/inorderTransform.c
+++ traversal/inorderTransform.c
@@ -1,73 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define SIZE 100
 
-struct Node{
-    char data;
-    struct Node *left;
-    struct Node *right;
-};
-struct Node *create(char x){
-    struct Node *newNode = malloc(sizeof(struct Node));
-    newNode -> data = x;
-    newNode -> left = NULL;
-    newNode -> right = NULL;
-}
-void inorder(struct Node *root){
-    if(root != NULL){
-        inorder(root -> left);
-        printf("%c ", root -> data);
-        inorder(root -> right);
-    }
-    return;
-}
-void preorder(struct Node *root){
-    if(root != NULL){
-        printf("%c ", root -> data);
-        preorder(root -> left);
-        preorder(root -> right);
-    }
-    return;
-}
-void postorder(struct Node *root){
-    if(root != NULL){
-        postorder(root -> left);
-        postorder(root -> right);
-        printf("%c ", root -> data);
-    }
-    return;
-}
-void printTree(){
-
+int isOperator(char c){
+    return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
 
 int main(){
-    /**char input[SIZE];
+    char input[SIZE];
     fgets(input, SIZE, stdin);
     input[strcspn(input, "\n")] = 0;
 
-    inorder(input);
-    preorder(input);
-    postorder(input);*/
 
-    struct Node *tree = create('A');
-    tree -> right = create('C');
-    tree -> left = create('B');
-    tree -> left -> left = create('D');
-    tree -> left -> left -> left = create('H');
-    tree -> left -> right = create('E');
-    tree -> left -> right -> right = create('I');
-    tree -> right -> left = create('F');
-    tree -> right -> left -> left= create('J');
-    tree -> right -> right = create('G');
+    char stack[SIZE];
+    int stackI = -1;
+    char output[SIZE];
+    int outputI = 0;
+    for (int i = 0; i < strlen(input); i++){
+        char c = input[i];
+        if (isOperator(c)){
+            if (c == '+' || c == '-'){
+                for ( ;stackI >= 0; stackI--){
+                    output[outputI++] = stack[stackI];
+                }
+                stack[++stackI] = c;
+            }else{
+                if (stackI > -1){
+                    if (stack[stackI] == '+' || stack[stackI] == '-'){
+                        stack[++stackI] = c;
+                    }else{
+                        output[outputI++] = stack[stackI];
+                        stack[stackI] = c;
+                    }
+                }else{
+                    output[outputI++] = stack[stackI];
+                    stack[stackI] = c;
+                }
+            }
+        }else{
+            output[outputI++] = c;
+        }
+    }
+    for (; stackI >= 0; stackI--){
+        output[outputI++] = stack[stackI];
+    }
+    output[outputI] = '\n';
+    printf("%s\n", output);
 
-    inorder(tree);
-    printf("\n");
-    preorder(tree);
-    printf("\n");
-    postorder(tree);
     return 0;
 }
 
